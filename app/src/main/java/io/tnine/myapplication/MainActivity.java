429266bd4ad4856b45cd0dd9c676e093d86d81d4
Aditya Chawla <aditya21196@gmail.com>
2015-12-15 16:42:21 +0530

    Intial Commit

---
+++ app/src/main/java/io/tnine/myapplication/MainActivity.java
@@ -0,0 +1,25 @@
+package io.tnine.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+        setContentView(R.layout.activity_main);
+
+
+
+
+    }
+}
