dc606c5ddd3d2f1023cb1681804b95581291d9f3
Aditya Chawla <aditya21196@gmail.com>
2015-12-26 12:26:25 +0530

    your location feature was set-up.

--- app/src/main/java/io/tnine/myapplication/fragment_console.java
+++ app/src/main/java/io/tnine/myapplication/fragment_console.java
@@ -3,6 +3,7 @@ package io.tnine.myapplication;
 import android.content.Context;
 import android.location.Address;
 import android.location.Geocoder;
+import android.location.Location;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Vibrator;
@@ -21,6 +22,9 @@ import android.widget.Toast;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.location.places.Place;
 import com.google.android.gms.location.places.Places;
 import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
@@ -35,12 +39,14 @@ import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.io.IOException;
+import java.text.DateFormat;
+import java.util.Date;
 import java.util.List;
 
 
 public class fragment_console extends AppCompatActivity implements
         GoogleApiClient.ConnectionCallbacks,
-        GoogleApiClient.OnConnectionFailedListener {
+        GoogleApiClient.OnConnectionFailedListener,LocationListener {
 
 
     //declaration of app constituents
@@ -50,7 +56,7 @@ public class fragment_console extends AppCompatActivity implements
     //if result is true, destination fragment is seen
     //if result is false, alarm settings fragment is seen
 
-    private GoogleApiClient mClient;
+    private GoogleApiClient mGoogleApiClient ;
 
 
     View frag1;
@@ -60,7 +66,20 @@ public class fragment_console extends AppCompatActivity implements
     double destlat;
     double destlng;
 
+    double latilast,longitlast;
+
     Marker marker;
+    Marker locmarker;
+
+    Location mLastLocation;
+    LocationRequest mLocationRequest;
+    Location mCurrentLocation;
+    String mLastUpdateTime;
+    String REQUESTING_LOCATION_UPDATES_KEY ;
+    boolean mRequestingLocationUpdates = true;
+    String LOCATION_KEY;
+    LatLng locll;
+
 
 
 
@@ -69,16 +88,17 @@ public class fragment_console extends AppCompatActivity implements
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fragment_console);
 
+        updateValuesFromBundle(savedInstanceState);
 
 
 
-        mClient = new GoogleApiClient
-                .Builder(this)
-                .addApi(Places.GEO_DATA_API)
-                .addApi(Places.PLACE_DETECTION_API)
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
-                .build();
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
 
         final PlaceAutocompleteFragment autocompleteFragment = (PlaceAutocompleteFragment)
                 getFragmentManager().findFragmentById(R.id.place_autocomplete_fragment);
@@ -170,7 +190,7 @@ public class fragment_console extends AppCompatActivity implements
         destlat = add.getLatitude();
         destlng = add.getLongitude();
 
-        gotoLocation(destlat, destlng, 15);
+        gotoLocation(destlat, destlng, 14);
         String locality = add.getLocality();
         if (marker != null) {
             marker.remove();
@@ -190,9 +210,37 @@ public class fragment_console extends AppCompatActivity implements
     }
 
     @Override
-    public void onConnected(Bundle bundle) {
+    public void onConnected(Bundle connectionHint) {
+       try{
+           mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
+                   mGoogleApiClient);
+           if (mLastLocation != null) {
 
+               latilast = mLastLocation.getLatitude();
+               longitlast = mLastLocation.getLongitude();
+               locll = new LatLng(latilast,longitlast);
+
+               MarkerOptions locoptions = new MarkerOptions()
+                       .title("You are here")
+                       .position(locll);
+               if (locmarker != null){
+                   locmarker.remove();
+               }
+               locmarker = map.addMarker(locoptions);
+               gotoLocation(latilast,longitlast,14);
+
+           }
+       }catch(SecurityException e){
+           //do nothing
+       }
+        if(mRequestingLocationUpdates){
+            createLocationRequest();
+            startLocationUpdates();
+        }
     }
+
+
+
 
     @Override
     public void onConnectionFailed(ConnectionResult connectionResult) {
@@ -209,67 +257,97 @@ public class fragment_console extends AppCompatActivity implements
     @Override
     protected void onStart() {
         super.onStart();
-        mClient.connect();
+        mGoogleApiClient.connect();
     }
 
     @Override
     protected void onStop() {
-        mClient.disconnect();
+        mGoogleApiClient .disconnect();
         super.onStop();
     }
 
+    protected void createLocationRequest() {
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(10000);
+        mLocationRequest.setFastestInterval(5000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
 
+    protected void startLocationUpdates() {
+
+        try{
+            LocationServices.FusedLocationApi.requestLocationUpdates(
+                    mGoogleApiClient, mLocationRequest, this);
+
+        }catch (SecurityException e){
+            //code to ask for permission
+        }
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        locll = new LatLng(location.getLatitude(),location.getLongitude());
+
+            MarkerOptions locoptions = new MarkerOptions()
+                    .title("You are here")
+                    .position(locll);
+        if(locmarker != null){
+            locmarker.remove();
+        }
+            locmarker = map.addMarker(locoptions);
+            startLocationUpdates();
+
+    }
+    //stopping location updates
+    @Override
+    protected void onPause() {
+        super.onPause();
+        stopLocationUpdates();
+    }
+
+    protected void stopLocationUpdates() {
+        LocationServices.FusedLocationApi.removeLocationUpdates(
+                mGoogleApiClient, this);
+    }
+
+    //resuming
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (mGoogleApiClient.isConnected() && !mRequestingLocationUpdates) {
+            startLocationUpdates();
+        }
+    }
+
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        savedInstanceState.putBoolean(REQUESTING_LOCATION_UPDATES_KEY,
+                mRequestingLocationUpdates);
+        savedInstanceState.putParcelable(LOCATION_KEY, mCurrentLocation);;
+        super.onSaveInstanceState(savedInstanceState);
+    }
+
+    private void updateValuesFromBundle(Bundle savedInstanceState) {
+        if (savedInstanceState != null) {
+            // Update the value of mRequestingLocationUpdates from the Bundle, and
+            // make sure that the Start Updates and Stop Updates buttons are
+            // correctly enabled or disabled.
+            if (savedInstanceState.keySet().contains(REQUESTING_LOCATION_UPDATES_KEY)) {
+                mRequestingLocationUpdates = savedInstanceState.getBoolean(
+                        REQUESTING_LOCATION_UPDATES_KEY);
+            }
+
+            // Update the value of mCurrentLocation from the Bundle and update the
+            // UI to show the correct latitude and longitude.
+            if (savedInstanceState.keySet().contains(LOCATION_KEY)) {
+                // Since LOCATION_KEY was found in the Bundle, we can be sure that
+                // mCurrentLocation is not null.
+                mCurrentLocation = savedInstanceState.getParcelable(LOCATION_KEY);
+            }
+
+        }
+    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
